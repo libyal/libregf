11c5a0f5ba63ab2cc91165d6cca54ff24347d98c
Joachim Metz <joachim.metz@gmail.com>
2015-01-05 08:28:48 +0100

    Applied updates.

--- libregf/regf_cell_values.h
+++ libregf/regf_cell_values.h
@@ -1,7 +1,7 @@
 /*
  * The cell values definition of a Windows NT Registry File (REGF)
  *
- * Copyright (C) 2009-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- regftools/regfexport.c
+++ regftools/regfexport.c
@@ -1,7 +1,7 @@
 /*
  * Exports information from a Windows NT Registry File (REGF)
  *
- * Copyright (C) 2009-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *

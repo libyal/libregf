1c1b9be479586d4fe6bd5aaf392ec105337859c4
Joachim Metz <joachim.metz@gmail.com>
2025-12-02 20:41:19 +0100

    Applied updates

--- acinclude.m4
+++ acinclude.m4
@@ -1,6 +1,6 @@
 dnl Checks for required headers and functions
 dnl
-dnl Version: 20230220
+dnl Version: 20240601
 
 dnl Function to detect if libregf dependencies are available
 AC_DEFUN([AX_LIBREGF_CHECK_LOCAL],
--- regftools/regfexport.c
+++ regftools/regfexport.c
@@ -1,7 +1,7 @@
 /*
  * Exports information from a Windows NT Registry File (REGF)
  *
- * Copyright (C) 2009-2024, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2025, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *

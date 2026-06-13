a9b796c9a6cc8f29fae81a3fa2a7763cebb8a30e
Joachim Metz <joachim.metz@gmail.com>
2026-06-13 09:14:32 +0200

    Replaced test scripts with autotest

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260602
+# Version: 20260609
 
 if test -f ${PWD}/libregf/.libs/libregf.1.dylib && test -f ./pyregf/.libs/pyregf.so
 then
 	install_name_tool -change /usr/local/lib/libregf.1.dylib ${PWD}/libregf/.libs/libregf.1.dylib ./pyregf/.libs/pyregf.so
 fi
 
+make check-build > /dev/null
+
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log
+if test ${RESULT} -ne 0
 then
-	cat tests/test-suite.log
+	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 

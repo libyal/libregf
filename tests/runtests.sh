dbcc36d2e550515edeea28f7c28393b346c03cfb
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 15:13:42 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libregf/.libs/libregf.1.dylib && test -f ./pyregf/.libs/pyregf.so
+if [ -f "${PWD}/libregf/.libs/libregf.1.dylib" ] && [ -f ./pyregf/.libs/pyregf.so ]
 then
-	install_name_tool -change /usr/local/lib/libregf.1.dylib ${PWD}/libregf/.libs/libregf.1.dylib ./pyregf/.libs/pyregf.so
+    install_name_tool -change /usr/local/lib/libregf.1.dylib "${PWD}/libregf/.libs/libregf.1.dylib" ./pyregf/.libs/pyregf.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
